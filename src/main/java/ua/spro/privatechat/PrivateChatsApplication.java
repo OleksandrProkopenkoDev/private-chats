873320ec2fa5ed213fcd7a80b700311d223f4314
Oleksandr Prokopenko <oleksandr.prokopenko@student.itmont.de>
2024-07-29 15:53:10 +0300

    init

---
+++ src/main/java/ua/spro/privatechat/PrivateChatsApplication.java
@@ -0,0 +1,13 @@
+package ua.spro.privatechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateChatsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PrivateChatsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ua/spro/privatechat/PrivateChatsApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.spro.privatechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateChatsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
